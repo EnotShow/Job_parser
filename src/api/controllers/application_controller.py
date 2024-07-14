5dd1828f688514840e17775d3591b16496b74b0b
Ihor <enotshow275@gmail.com>
2024-07-14 23:17:01 +0200

    delete usless methood

--- src/api/controllers/application_controller.py
+++ src/api/controllers/application_controller.py
@@ -10,8 +10,7 @@ from starlette.status import HTTP_400_BAD_REQUEST
 
 from core.shared.errors import NoRowsFoundError
 from src.api.containers.services_containers.application_service_container import ApplicationServiceContainer
-from src.api.dtos.application_dto import ApplicationFilterDTO, ApplicationDTO, ApplicationCreateDTO, \
-    ApplicationUpdateDTO
+from src.api.dtos.application_dto import ApplicationFilterDTO, ApplicationDTO, ApplicationUpdateDTO
 from src.api.services.application_service import ApplicationService
 
 router = APIRouter(prefix="/application", tags=["application"])
@@ -41,18 +40,6 @@ async def create_multiple_applications(
         return await application_service.create_multiple_applications(
             data if isinstance(data, list) else [data]
         )
-    except NoRowsFoundError:
-        raise HTTPException(HTTP_400_BAD_REQUEST, {'data': 'No rows found'})
-
-
-@router.post("/user_applications", status_code=status.HTTP_201_CREATED)
-@inject
-async def get_user_applied_applications(
-        data: ApplicationCreateDTO,
-        application_service: ApplicationService = Depends(Provide[ApplicationServiceContainer.application_service]),
-):
-    try:
-        return await application_service.create_application(data)
     except NoRowsFoundError:
         raise HTTPException(HTTP_400_BAD_REQUEST, {'data': 'No rows found'})
 
