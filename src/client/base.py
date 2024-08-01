17238a6f32d00e476fb8316c449bc1552753d383
Ihor <enotshow275@gmail.com>
2024-08-01 22:45:17 +0200

    client_register

--- src/client/base.py
+++ src/client/base.py
@@ -1,6 +1,7 @@
 import httpx
 
 from core.config.proj_settings import settings
+from src.api.auth.auth_dto import UserRegisterDTO, UserLoginDTO
 
 
 class JobParserClient:
@@ -13,16 +14,18 @@ class JobParserClient:
         self.session = httpx.AsyncClient()
         self.refresh_token = None
 
-    async def auth_as_user(self, email: str, password: str):
+    async def auth_as_user(self, data: UserLoginDTO):
         tokens = await self.session.post(
             f"{self.base_url}/auth/login",
-            json={"email": email, "password": password},
+            json=data.model_dump(),
         )
         if tokens.status_code != 200:
             raise Exception
 
         self.session.headers["Authorization"] = f"Bearer {tokens.json()['access_token']}"
         self.refresh_token = tokens.json()["refresh_token"]
+
+        return tokens
 
     async def refresh_access_token(self):
         tokens = await self.session.post(
@@ -36,6 +39,8 @@ class JobParserClient:
         print(tokens.json())
         self.refresh_token = tokens.json()["access_token"]
 
+        return tokens
+
     async def verify_token(self):
         tokens = await self.session.get(
             f"{self.base_url}/auth/verify_token",
@@ -44,3 +49,13 @@ class JobParserClient:
             raise Exception
 
         return True if tokens.status_code == 200 else False
+
+    async def register(self, data: UserRegisterDTO):
+        tokens = await self.session.post(
+            f"{self.base_url}/auth/register",
+            json=data.model_dump(),
+        )
+        if tokens.status_code != 200:
+            raise Exception
+
+        return tokens
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,11 +1,14 @@
 import asyncio
 
+from src.api.auth.auth_dto import UserLoginDTO, UserRegisterDTO
+from src.api.users.user_dto import UserCreateDTO
 from src.client.base import JobParserClient
 
 
 async def test_auth():
     client = JobParserClient()
-    await client.auth_as_user("enotshow275@gmail.com", "Bunograd")
+    user = UserLoginDTO(email="testuser@mail.com", password="TestPassword")
+    await client.auth_as_user(user)
 
     assert client.session.headers["Authorization"]
     assert await client.verify_token()
@@ -13,3 +16,15 @@ async def test_auth():
     await client.refresh_access_token()
     assert client.session.headers["Authorization"]
     assert await client.verify_token()
+
+
+async def test_create_user():
+    client = JobParserClient()
+    user = UserRegisterDTO(
+        email="testuser@mail.com",
+        password="TestPassword",
+        language_code="en",
+        refer_id=0,
+    )
+    r = await client.register(user)
+    assert r.status_code == 200
