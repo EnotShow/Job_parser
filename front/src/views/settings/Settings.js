d1b272ef3ffd93a798cc7e48285472492c6d17ce
Ihor <enotshow275@gmail.com>
2024-08-08 23:11:35 +0200

    settings template

--- front/src/views/settings/Settings.js
+++ front/src/views/settings/Settings.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 import {
-  CButton,
+  CButton, CButtonGroup,
   CCard,
   CCardBody,
   CCardHeader,
-  CCol, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle, CFormCheck, CFormSelect, CFormSwitch,
+  CCol,
+  CDropdown,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
+  CForm,
+  CFormCheck, CFormInput,
+  CFormLabel,
+  CFormSelect,
+  CFormSwitch, CInputGroup, CInputGroupText,
   CRow,
 } from '@coreui/react'
 
@@ -16,21 +25,62 @@ const Settings = () => {
       <CRow>
         <CCol xs={12}>
           <CCard className="mb-4">
-            <CCardHeader>Settings</CCardHeader>
             <CCardBody>
-              <CFormCheck id="flexCheckDefault" label="Default checkbox"/>
-              <CFormCheck id="flexCheckChecked" label="Checked checkbox" defaultChecked />
-              <CFormSwitch label="Default switch checkbox input" id="formSwitchCheckDefault"/>
-              <CFormSwitch label="Checked switch checkbox input" id="formSwitchCheckChecked" defaultChecked/>
-              <CFormSelect id="inputState" label="State">
-                <option>Choose...</option>
-                <option>...</option>
-              </CFormSelect>
               <CRow>
                 <CCol>
-                  <CButton color="primary">Save</CButton>
+                  <center><h1>Settings</h1></center>
                 </CCol>
               </CRow>
+              <CForm className="row g-3 mt-4">
+                {/* Language Selector */}
+                <CCol md={12}>
+                  <CFormLabel htmlFor="languageSelect">Language</CFormLabel>
+                  <CFormSelect id="languageSelect" aria-label="Language select">
+                    <option value="en">English</option>
+                    <option value="pl">Polish</option>
+                    <option value="ua">Ukrainian</option>
+                    <option value="ru">Russian</option>
+                  </CFormSelect>
+                </CCol>
+
+                {/* Pause Parsing Switch */}
+                <CCol md={12} className="d-flex align-items-center">
+                  <CFormLabel htmlFor="pauseParsingButton" className="me-4" style={{ fontSize: '1.25rem' }}>Pause Parsing</CFormLabel>
+                  <CButtonGroup>
+                    <CButton
+                      color="primary"
+                      variant="outline"
+                      id="pauseParsingButton"
+                      active
+                      onClick={() => console.log('Parsing Paused')}
+                    >
+                      Pause
+                    </CButton>
+                    <CButton
+                      color="secondary"
+                      variant="outline"
+                      onClick={() => console.log('Parsing Resumed')}
+                    >
+                      Resume
+                    </CButton>
+                  </CButtonGroup>
+                </CCol>
+
+                {/* Links Limit with Increase Button */}
+                <CCol md={12}>
+                  <CFormLabel htmlFor="linksLimit">Links Limit</CFormLabel>
+                  <CInputGroup>
+                    <CFormInput type="number" id="linksLimit" disabled defaultValue="25" />
+                    <CInputGroupText>
+                      <CButton color="primary">Increase</CButton>
+                    </CInputGroupText>
+                  </CInputGroup>
+                </CCol>
+
+                <CCol xs={12} className="mt-4">
+                  <CButton color="primary" type="submit">Save Settings</CButton>
+                </CCol>
+              </CForm>
             </CCardBody>
           </CCard>
         </CCol>
