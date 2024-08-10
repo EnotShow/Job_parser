89dbf0c4e8e13061ad0cbbaeb29e2ac7fe71bb61
Ihor <88674695+EnotShow@users.noreply.github.com>
2024-08-10 13:00:55 +0200

    actualization of parsers

---
+++ src/parsers/pracuj_pl.py
@@ -0,0 +1,53 @@
+import asyncio
+
+import requests
+from bs4 import BeautifulSoup
+
+from src.api.applications.application_dto import ApplicationCreateDTO
+
+
+class PracujParser:
+    base_url = 'https://www.pracuj.pl'
+
+    async def parse_offers(self, url: str, owner_id: int):
+        result = []
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "html.parser")
+        offers_section = soup.find('div', {'data-test': 'section-offers'})
+        if offers_section:
+            jobs = offers_section.find_all(class_='tiles_b1j1pbod')
+            for i in jobs:
+                description = await self._parse_details(i.a['href'])
+                job = ApplicationCreateDTO(
+                    title=i.get_text(separator='\n'),
+                    url=i.a['href'],
+                    description=description,
+                    owner_id=owner_id
+                )
+                result.append(job)
+        return result
+
+    async def _parse_details(self, url: str):
+        def extract_section_text(soup, section_name):
+            section = soup.find('section', {'data-test': section_name})
+            return section.get_text(separator="\n") if section else ""
+
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "html.parser")
+
+        responsibilities = extract_section_text(soup, 'section-responsibilities')
+        requirements = extract_section_text(soup, 'section-requirements')
+        offered = extract_section_text(soup, 'section-offered')
+
+        description = f"{responsibilities}\n{requirements}\n{offered}"
+        return description
+
+
+async def main():
+    parser = PracujParser()
+    res = await parser.parse_offers('https://it.pracuj.pl/praca/linux;kw?sc=0', 1)
+    print(f"{res}'\n'{len(res)}")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
