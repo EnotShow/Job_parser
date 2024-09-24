c4a80ef01e5b517ed7e516ffec7ab03ebe6df5b4
Ihor <enotshow275@gmail.com>
2024-09-24 23:38:59 +0200

    run-worker-healthcheck

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,6 +1,7 @@
 import click
 
 from src.cli.add_user import add_user_async
+from src.cli.run_worker_healthcheck import run_worker_healthcheck
 from src.cli.runserver import runserver
 
 
@@ -10,5 +11,6 @@ def cli():
 
 
 def add_cli_commands():
-    cli.add_command(add_user_async)
     cli.add_command(runserver)
+    cli.add_command(add_user_async)
+    cli.add_command(run_worker_healthcheck)
